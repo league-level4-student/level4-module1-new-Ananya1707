7e74321addb394e9176fc99ed6eb131c7310db33
league <league@imac-14.attlocal.net>
2023-06-01 18:29:40 -0700

    working on polymorph

--- src/_05_Polymorphs/MovingMorph.java
+++ src/_05_Polymorphs/MovingMorph.java
@@ -1,11 +1,10 @@
 package _05_Polymorphs;
 
-import java.awt.Color;
 import java.awt.Graphics;
 
-public class RedPolymorph extends Polymorph {
+public class MovingMorph extends Polymorph {
 
-	public RedPolymorph(int x, int y) {
+	public MovingMorph(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
 	}
@@ -19,8 +18,7 @@ public class RedPolymorph extends Polymorph {
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
-		g.setColor(Color.red);
-		g.fillRect(x, y, getWidth(), getHeight());
+		
 		
 	}
 
